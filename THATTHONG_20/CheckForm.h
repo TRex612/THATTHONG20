be6662508adfd58184698375c86a42d4989c9da5
Aumboonaew <unaewblanca@gmail.com>
2023-03-15 23:36:51 +0700

    commitchecks

--- THATTHONG_20/CheckForm.h
+++ THATTHONG_20/CheckForm.h
@@ -42,6 +42,9 @@ namespace THATTHONG20 {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Panel^ panel1;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+
 
 	private:
 		/// <summary>
@@ -64,55 +67,66 @@ namespace THATTHONG20 {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button2
 			// 
-			this->button2->Anchor = System::Windows::Forms::AnchorStyles::Top;
-			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
-				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button2->Anchor = System::Windows::Forms::AnchorStyles::Right;
+			this->button2->BackColor = System::Drawing::Color::LightCoral;
 			this->button2->Font = (gcnew System::Drawing::Font(L"Tahoma", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(506, 193);
+			this->button2->Location = System::Drawing::Point(978, 293);
+			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button2->MinimumSize = System::Drawing::Size(120, 46);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(90, 37);
+			this->button2->Size = System::Drawing::Size(120, 46);
 			this->button2->TabIndex = 15;
 			this->button2->Text = L"CLEAR";
 			this->button2->UseVisualStyleBackColor = false;
 			// 
 			// button1
 			// 
-			this->button1->Anchor = System::Windows::Forms::AnchorStyles::Top;
-			this->button1->BackColor = System::Drawing::Color::LimeGreen;
+			this->button1->Anchor = System::Windows::Forms::AnchorStyles::Right;
+			this->button1->BackColor = System::Drawing::Color::DarkSeaGreen;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Tahoma", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(352, 193);
+			this->button1->Location = System::Drawing::Point(850, 293);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button1->MinimumSize = System::Drawing::Size(120, 46);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(90, 37);
+			this->button1->Size = System::Drawing::Size(120, 46);
 			this->button1->TabIndex = 14;
 			this->button1->Text = L"CHECK";
 			this->button1->UseVisualStyleBackColor = false;
 			// 
 			// textBox1
 			// 
-			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
+			this->textBox1->Anchor = System::Windows::Forms::AnchorStyles::Right;
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Tahoma", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(287, 83);
+			this->textBox1->Location = System::Drawing::Point(719, 213);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->textBox1->MinimumSize = System::Drawing::Size(379, 51);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(399, 43);
+			this->textBox1->Size = System::Drawing::Size(379, 51);
 			this->textBox1->TabIndex = 13;
 			// 
 			// label3
 			// 
-			this->label3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
+			this->label3->Anchor = System::Windows::Forms::AnchorStyles::Right;
 			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Angsana New", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label3->BackColor = System::Drawing::Color::Black;
+			this->label3->Font = (gcnew System::Drawing::Font(L"supermarket", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(109, 78);
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(682, 120);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 30);
+			this->label3->MinimumSize = System::Drawing::Size(117, 59);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(93, 44);
+			this->label3->Size = System::Drawing::Size(117, 59);
 			this->label3->TabIndex = 12;
 			this->label3->Text = L"เลขพัสดุ";
 			// 
@@ -121,53 +135,73 @@ namespace THATTHONG20 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Angsana New", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(19, 18);
+			this->label1->Location = System::Drawing::Point(17, 19);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(49, 30);
+			this->label1->Size = System::Drawing::Size(62, 37);
 			this->label1->TabIndex = 10;
 			this->label1->Text = L"ข้อมูล";
 			// 
 			// panel1
 			// 
-			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
+			this->panel1->Anchor = System::Windows::Forms::AnchorStyles::Right;
+			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->panel1->Controls->Add(this->label1);
 			this->panel1->Font = (gcnew System::Drawing::Font(L"Angsana New", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->panel1->Location = System::Drawing::Point(287, 277);
-			this->panel1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->panel1->Location = System::Drawing::Point(719, 369);
+			this->panel1->Margin = System::Windows::Forms::Padding(3, 5, 300, 5);
+			this->panel1->MinimumSize = System::Drawing::Size(379, 266);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(492, 216);
+			this->panel1->Size = System::Drawing::Size(379, 266);
 			this->panel1->TabIndex = 16;
 			// 
 			// button3
 			// 
+			this->button3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
 			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
 			this->button3->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->button3->Location = System::Drawing::Point(11, 517);
-			this->button3->Margin = System::Windows::Forms::Padding(2);
+			this->button3->Location = System::Drawing::Point(15, 626);
+			this->button3->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(44, 50);
+			this->button3->Size = System::Drawing::Size(79, 73);
 			this->button3->TabIndex = 152;
 			this->button3->UseVisualStyleBackColor = true;
 			this->button3->Click += gcnew System::EventHandler(this, &CheckForm::button3_Click);
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(0, 3);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(3, 3, 3, 8);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(1205, 758);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 153;
+			this->pictureBox1->TabStop = false;
+			// 
 			// CheckForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(890, 578);
+			this->ClientSize = System::Drawing::Size(1187, 711);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->panel1);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
-			this->MinimumSize = System::Drawing::Size(906, 617);
+			this->Controls->Add(this->pictureBox1);
+			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->MinimumSize = System::Drawing::Size(1202, 749);
 			this->Name = L"CheckForm";
 			this->Text = L"CheckForm";
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
