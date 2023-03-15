0cd1627e8ec3b05b7608eb94f91e03fdb42d69e0
Aumboonaew <unaewblanca@gmail.com>
2023-03-15 14:06:40 +0700

    commit

--- THATTHONG_20/RegisForm.cpp
+++ THATTHONG_20/RegisForm.cpp
@@ -1,13 +1,1 @@
 #include "RegisForm.h"
-
-using namespace System;
-using namespace System::Windows::Forms;
-void main(array<String^>^ args)
-{
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	THATHONG20::RegisForm from;
-	Application::Run(% form);
-}
-
-
