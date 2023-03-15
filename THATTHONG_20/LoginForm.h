7a964e0fdf3c7dd4924d3562c2d955d1d6984559
Aumboonaew <unaewblanca@gmail.com>
2023-03-16 00:07:47 +0700

    commitlogin

--- THATTHONG_20/LoginForm.h
+++ THATTHONG_20/LoginForm.h
@@ -43,6 +43,8 @@ namespace THATTHONG20 {
 	private: System::Windows::Forms::Button^ OKlogin;
 	private: System::Windows::Forms::TextBox^ Passlogin;
 	private: System::Windows::Forms::TextBox^ IDlogin;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label1;
 
 	private:
 		/// <summary>
@@ -57,6 +59,7 @@ namespace THATTHONG20 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(LoginForm::typeid));
 			this->PASSWORD = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -64,18 +67,23 @@ namespace THATTHONG20 {
 			this->OKlogin = (gcnew System::Windows::Forms::Button());
 			this->Passlogin = (gcnew System::Windows::Forms::TextBox());
 			this->IDlogin = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// PASSWORD
 			// 
 			this->PASSWORD->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
 			this->PASSWORD->AutoSize = true;
-			this->PASSWORD->Font = (gcnew System::Drawing::Font(L"Tahoma", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->PASSWORD->BackColor = System::Drawing::Color::Black;
+			this->PASSWORD->Font = (gcnew System::Drawing::Font(L"supermarket", 22.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->PASSWORD->Location = System::Drawing::Point(165, 370);
+			this->PASSWORD->ForeColor = System::Drawing::Color::White;
+			this->PASSWORD->Location = System::Drawing::Point(231, 387);
 			this->PASSWORD->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->PASSWORD->Name = L"PASSWORD";
-			this->PASSWORD->Size = System::Drawing::Size(193, 36);
+			this->PASSWORD->Size = System::Drawing::Size(191, 68);
 			this->PASSWORD->TabIndex = 17;
 			this->PASSWORD->Text = L"PASSWORD";
 			// 
@@ -83,25 +91,29 @@ namespace THATTHONG20 {
 			// 
 			this->label3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
 			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Tahoma", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label3->BackColor = System::Drawing::Color::Black;
+			this->label3->Font = (gcnew System::Drawing::Font(L"supermarket", 22.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(259, 270);
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(309, 315);
 			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(113, 36);
+			this->label3->Size = System::Drawing::Size(124, 68);
 			this->label3->TabIndex = 16;
 			this->label3->Text = L"NAME ";
 			// 
 			// label2
 			// 
-			this->label2->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
 			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Tahoma", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label2->BackColor = System::Drawing::Color::Black;
+			this->label2->Font = (gcnew System::Drawing::Font(L"supermarket", 49.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(479, 103);
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(498, 152);
 			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(182, 57);
+			this->label2->Size = System::Drawing::Size(251, 145);
 			this->label2->TabIndex = 15;
 			this->label2->Text = L"LOGIN";
 			this->label2->TextAlign = System::Drawing::ContentAlignment::TopCenter;
@@ -109,25 +121,33 @@ namespace THATTHONG20 {
 			// Cancellogin
 			// 
 			this->Cancellogin->Anchor = System::Windows::Forms::AnchorStyles::Top;
-			this->Cancellogin->Location = System::Drawing::Point(661, 511);
-			this->Cancellogin->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->Cancellogin->BackColor = System::Drawing::Color::BlueViolet;
+			this->Cancellogin->Font = (gcnew System::Drawing::Font(L"supermarket", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Cancellogin->ForeColor = System::Drawing::Color::White;
+			this->Cancellogin->Location = System::Drawing::Point(888, 471);
+			this->Cancellogin->Margin = System::Windows::Forms::Padding(4);
 			this->Cancellogin->Name = L"Cancellogin";
-			this->Cancellogin->Size = System::Drawing::Size(213, 89);
+			this->Cancellogin->Size = System::Drawing::Size(80, 43);
 			this->Cancellogin->TabIndex = 14;
 			this->Cancellogin->Text = L"Cancel";
-			this->Cancellogin->UseVisualStyleBackColor = true;
+			this->Cancellogin->UseVisualStyleBackColor = false;
 			this->Cancellogin->Click += gcnew System::EventHandler(this, &LoginForm::Cancellogin_Click);
 			// 
 			// OKlogin
 			// 
 			this->OKlogin->Anchor = System::Windows::Forms::AnchorStyles::Top;
-			this->OKlogin->Location = System::Drawing::Point(327, 511);
-			this->OKlogin->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->OKlogin->BackColor = System::Drawing::Color::DimGray;
+			this->OKlogin->Font = (gcnew System::Drawing::Font(L"supermarket", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->OKlogin->ForeColor = System::Drawing::Color::White;
+			this->OKlogin->Location = System::Drawing::Point(790, 472);
+			this->OKlogin->Margin = System::Windows::Forms::Padding(4);
 			this->OKlogin->Name = L"OKlogin";
-			this->OKlogin->Size = System::Drawing::Size(213, 89);
+			this->OKlogin->Size = System::Drawing::Size(80, 43);
 			this->OKlogin->TabIndex = 12;
 			this->OKlogin->Text = L"Login";
-			this->OKlogin->UseVisualStyleBackColor = true;
+			this->OKlogin->UseVisualStyleBackColor = false;
 			this->OKlogin->Click += gcnew System::EventHandler(this, &LoginForm::OKlogin_Click);
 			// 
 			// Passlogin
@@ -135,30 +155,60 @@ namespace THATTHONG20 {
 			this->Passlogin->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
 			this->Passlogin->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->Passlogin->Location = System::Drawing::Point(477, 368);
-			this->Passlogin->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->Passlogin->Location = System::Drawing::Point(430, 401);
+			this->Passlogin->Margin = System::Windows::Forms::Padding(4);
 			this->Passlogin->Name = L"Passlogin";
 			this->Passlogin->PasswordChar = '*';
-			this->Passlogin->Size = System::Drawing::Size(543, 38);
+			this->Passlogin->Size = System::Drawing::Size(538, 38);
 			this->Passlogin->TabIndex = 13;
 			this->Passlogin->UseSystemPasswordChar = true;
+			this->Passlogin->TextChanged += gcnew System::EventHandler(this, &LoginForm::Passlogin_TextChanged);
 			// 
 			// IDlogin
 			// 
 			this->IDlogin->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom));
 			this->IDlogin->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->IDlogin->Location = System::Drawing::Point(477, 268);
-			this->IDlogin->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->IDlogin->Location = System::Drawing::Point(430, 328);
+			this->IDlogin->Margin = System::Windows::Forms::Padding(4);
 			this->IDlogin->Name = L"IDlogin";
-			this->IDlogin->Size = System::Drawing::Size(543, 38);
+			this->IDlogin->Size = System::Drawing::Size(538, 38);
 			this->IDlogin->TabIndex = 11;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->pictureBox1->BackColor = System::Drawing::Color::Black;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(-14, -5);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(1205, 758);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 18;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Black;
+			this->label1->Font = (gcnew System::Drawing::Font(L"supermarket", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::DimGray;
+			this->label1->Location = System::Drawing::Point(528, 563);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(173, 44);
+			this->label1->TabIndex = 19;
+			this->label1->Text = L"THATTHONG SAO";
 			// 
 			// LoginForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1187, 711);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->PASSWORD);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
@@ -166,10 +216,11 @@ namespace THATTHONG20 {
 			this->Controls->Add(this->OKlogin);
 			this->Controls->Add(this->Passlogin);
 			this->Controls->Add(this->IDlogin);
+			this->Controls->Add(this->pictureBox1);
 			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			this->MinimumSize = System::Drawing::Size(1202, 749);
 			this->Name = L"LoginForm";
 			this->Text = L" ";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -209,6 +260,8 @@ namespace THATTHONG20 {
 	}
 private: System::Void Cancellogin_Click(System::Object^ sender, System::EventArgs^ e) {
 	Application::Exit();
+}
+private: System::Void Passlogin_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
